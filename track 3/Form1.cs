f9c29f064d688e722986028072c0d3748acc9900
ryers <ryers@GREG-PC>
2023-06-18 14:02:31 -0400

    Add project files.

---
+++ track 3/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace track_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //track Pen
+        Pen WhitePen = new Pen(Color.WhiteSmoke, 5);
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
